c57d303d57ce5bc6f1db79d2f35c3bee475f11c5
David Phan <dphan015@ucr.edu>
2018-07-08 19:53:20 -0700

    converted to open class to mock

--- data/src/main/java/com/dphan/data/mapper/ProjectMapper.kt
+++ data/src/main/java/com/dphan/data/mapper/ProjectMapper.kt
@@ -4,7 +4,7 @@ import com.dphan.data.model.ProjectEntity
 import com.dphan.domain.model.Project
 import javax.inject.Inject
 
-class ProjectMapper @Inject constructor() : EntityMapper<ProjectEntity, Project> {
+open class ProjectMapper @Inject constructor() : EntityMapper<ProjectEntity, Project> {
 
     override fun mapFromEntity(entity: ProjectEntity): Project {
         return Project(entity.id, entity.name, entity.fullName, entity.starCount,
