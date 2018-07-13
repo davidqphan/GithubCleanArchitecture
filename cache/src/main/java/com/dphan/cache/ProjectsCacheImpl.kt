4fca44f6da031831764bcf63f64a9e2aad99362e
David Phan <dphan015@ucr.edu>
2018-07-12 22:36:14 -0700

    implementation of data module's ProjectsCache interface

---
+++ cache/src/main/java/com/dphan/cache/ProjectsCacheImpl.kt
@@ -0,0 +1,85 @@
+package com.dphan.cache
+
+import com.dphan.cache.db.ProjectsDatabase
+import com.dphan.cache.mapper.CachedProjectMapper
+import com.dphan.cache.model.Config
+import com.dphan.data.model.ProjectEntity
+import com.dphan.data.repository.ProjectsCache
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.rxkotlin.toSingle
+import javax.inject.Inject
+
+class ProjectsCacheImpl @Inject constructor(
+        private val projectsDatabase: ProjectsDatabase,
+        private val mapper: CachedProjectMapper) : ProjectsCache {
+
+    override fun clearProjects(): Completable {
+        return Completable.defer {
+            projectsDatabase.cachedProjectsDao().deleteProjects()
+            Completable.complete()
+        }
+    }
+
+    override fun saveProjects(projects: List<ProjectEntity>): Completable {
+        return Completable.defer {
+            projectsDatabase.cachedProjectsDao().insertProjects(
+                    projects.map { mapper.mapToCached(it) })
+            Completable.complete()
+        }
+    }
+
+    override fun getProjects(): Observable<List<ProjectEntity>> {
+        return projectsDatabase.cachedProjectsDao().getProjects()
+                .toObservable()
+                .map {
+                    it.map { mapper.mapFromCached(it) }
+                }
+    }
+
+    override fun getBookmarkedProjects(): Observable<List<ProjectEntity>> {
+        return projectsDatabase.cachedProjectsDao().getBookmarkedProjects()
+                .toObservable()
+                .map {
+                    it.map { mapper.mapFromCached(it) }
+                }
+    }
+
+    override fun setProjectAsBookmarked(projectId: String): Completable {
+        return Completable.defer {
+            projectsDatabase.cachedProjectsDao().setBookmarkStatus(true, projectId)
+            Completable.complete()
+        }
+    }
+
+    override fun setProjectAsNotBookmarked(projectId: String): Completable {
+        return Completable.defer {
+            projectsDatabase.cachedProjectsDao().setBookmarkStatus(false, projectId)
+            Completable.complete()
+        }
+    }
+
+    override fun areProjectsCached(): Single<Boolean> {
+        return projectsDatabase.cachedProjectsDao().getProjects().isEmpty
+                .map { !it }
+    }
+
+    override fun setLastCacheTime(lastCache: Long): Completable {
+        return Completable.defer {
+            projectsDatabase.configDao().insertConfig(Config(lastCacheTime = lastCache))
+            Completable.complete()
+        }
+    }
+
+    override fun isProjectsCacheExpired(): Single<Boolean> {
+        val currentTime = System.currentTimeMillis()
+        val expirationTime = (60 * 10 * 1000).toLong()
+        return projectsDatabase.configDao().getConfig()
+                .toSingle()
+                .map {
+                    currentTime - it.lastCacheTime > expirationTime
+                }
+    }
+
+}
