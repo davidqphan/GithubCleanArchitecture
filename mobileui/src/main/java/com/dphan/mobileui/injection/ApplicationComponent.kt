d5cd23ecac37989a33911a963ac20222151a0e5b
David Phan <dphan015@ucr.edu>
2018-07-17 21:37:52 -0700

    used by Dagger to generate the code to fulfill the required dependencies

---
+++ mobileui/src/main/java/com/dphan/mobileui/injection/ApplicationComponent.kt
@@ -0,0 +1,34 @@
+package com.dphan.mobileui.injection
+
+import android.app.Application
+import com.dphan.mobileui.GithubTrendingApplication
+import com.dphan.mobileui.injection.module.*
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = arrayOf(
+        AndroidInjectionModule::class,
+        ApplicationModule::class,
+        UiModule::class,
+        PresentationModule::class,
+        DataModule::class,
+        CacheModule::class,
+        RemoteModule::class))
+interface ApplicationComponent {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): ApplicationComponent
+
+    }
+
+    fun inject(app: GithubTrendingApplication)
+
+}
